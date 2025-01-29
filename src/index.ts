764e00d64e08eab6c335e3cd4988b137c9f1fe75
Alejandro <alejandro@jrmendonca.com.br>
2025-01-28 22:54:21 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
 import sequelize from './config/database';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 sequelize
